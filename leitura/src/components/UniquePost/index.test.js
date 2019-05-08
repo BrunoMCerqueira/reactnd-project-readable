4d1e969715f24d5b81489f8fea2ec9123ce9e8f0
Bruno Cerqueira <brunocerqueiram@gmail.com>
2019-05-08 11:35:21 -0300

    Fix Unique Post and Wrap Platform tests.

--- leitura/src/components/UniquePost/index.test.js
+++ leitura/src/components/UniquePost/index.test.js
@@ -5,7 +5,19 @@ import UniquePost from '../UniquePost'
 
 describe('Test UniquePost component', () => {
   let wrapper;
-  beforeEach(()=> { wrapper = mount(<UniquePost />)})
+  const post = {
+    author: "thingone",
+    body: "Just kidding. It takes more than 10 minutes to learn technology.",
+    category: "redux",
+    commentCount: 0,
+    deleted: false,
+    id: "6ni6ok3ym7mf1p33lnez",
+    timestamp: 1468479767190,
+    title: "Learn Redux in 10 minutes!",
+    voteScore: -5,
+  }
+
+  beforeEach(()=> { wrapper = mount(<UniquePost post={post} />)})
 
   it('renders a card', () => {
     expect(wrapper.find('.card')).toHaveLength(1);
--- leitura/src/components/WrapPlatform/index.test.js
+++ leitura/src/components/WrapPlatform/index.test.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { mount } from 'enzyme';
 import WrapPlatform from '../WrapPlatform'
 import { MemoryRouter } from 'react-router-dom';
+import thunk from "redux-thunk";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from "redux";
+import reducer from '../../reducer';
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 describe('Test WrapPlatform component', () => {
+  const match = { params: { category: 'react' } }
   let wrapper;
-  beforeEach(()=> { wrapper = mount(<MemoryRouter><WrapPlatform /></MemoryRouter>)})
+  beforeEach(()=> { wrapper = mount(<Provider store={store}><MemoryRouter><WrapPlatform match={match} /></MemoryRouter></Provider>)})
 
   it('renders a nav', () => {
     expect(wrapper.find('nav')).toHaveLength(1);    
@@ -15,4 +22,3 @@ describe('Test WrapPlatform component', () => {
     expect(wrapper.find('.fa-plus')).toHaveLength(1);
   })
 })
-
